2c2a0c3e538def93ecf76a652d5b3f899dc3cec5
darkguyidk <93890773+darkguyidk@users.noreply.github.com>
2026-03-18 06:01:43 -0700

    Create buildsite.js

---
+++ buildsite.js
@@ -0,0 +1,17 @@
+document.writeln("<!DOCTYPE html>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("<title>Ralsei's cool playfab spammer</title>");
+document.writeln("</head>");
+document.writeln("<body>");
+document.writeln("<h1>Ralsei's cool playfab spammer</h1>");
+document.writeln("<p>TitleId:</p>");
+document.writeln("<input type=\"text\" id=\"titleIdInput\">");
+document.writeln("<p>Amount of accounts:</p>");
+document.writeln("<input type=\"number\" id=\"amountOfAccounts\"><br>");
+document.writeln("<button id=\"doTheThing\">Spam</button>");
+document.writeln("<p>Output:</p>");
+document.writeln("<p style=\"border: 2px solid black;\" id=\"outputBox\"></p>");
+document.writeln("</body>");
+document.writeln("</html>");
+import "https://github.com/darkguyidk/playfabspammer/raw/refs/heads/main/playfabspammer.js"
